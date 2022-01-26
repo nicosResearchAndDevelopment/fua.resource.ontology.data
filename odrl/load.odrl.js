eb27fc927ad88cf61361cdbad7585b032fd5ed1b
Simon Petrac <pfoerdie@gmail.com>
2022-01-26 16:07:13 +0100

    fixed typo

--- odrl/load.odrl.js
+++ odrl/load.odrl.js
@@ -4,8 +4,8 @@ module.exports = {
     '@context':        'fua.load.rdf',
     'dct:identifier':  path.join(__dirname, './load.odrl.js'),
     'dct:format':      'application/fua.load+js',
-    'dct:title':       'load.ldp',
-    'dct:alternative': '@nrd/fua.resource.ontology/ldp',
+    'dct:title':       'load.odrl',
+    'dct:alternative': '@nrd/fua.resource.ontology/odrl',
     'dct:requires':    [{
         'dct:identifier': './local/odrl.ttl',
         'dct:format':     'text/turtle'
